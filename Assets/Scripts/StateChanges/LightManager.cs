daae605f3510ce7127599ae5f8ffc4be23e0da9d
MelDidntListen <melodyisnotlistening@gmail.com>
2026-03-23 17:21:47 -0700

    Light Manager + Temp Assets

---
+++ Assets/Scripts/StateChanges/LightManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+
+{
+    [Header("Light Controls")]
+    private float brightness;
+    private float flickerSpeed; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BaseLight()
+    {
+        
+    }
+
+    void PartyTime()
+    {
+        
+    }
+
+    void HauntRed()
+    {
+        
+    }
+
+    void HauntFlicker()
+    {
+        
+    }
+}
